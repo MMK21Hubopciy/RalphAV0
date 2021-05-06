1fe540b7e7e0504588e36d4c6d7c0b1daf26f305
MMK21Hubopciy <knoarb99@163.com>
2021-05-07 07:07:17 +0800

    Added main framework for Desktop and Android

---
+++ android/src/com/paladinzzz/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.paladinzzz.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.paladinzzz.game.CrossplatformApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CrossplatformApp(), config);
+	}
+}
---
+++ desktop/src/com/paladinzzz/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.paladinzzz.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.paladinzzz.game.CrossplatformApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrossplatformApp(), config);
+	}
+}
