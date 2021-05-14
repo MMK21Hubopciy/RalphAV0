2713753c784b179ef65d7a1629d1c501330f720a
MMK21Hubopciy <knoarb99@163.com>
2021-05-14 18:46:10 +0800

    Added static ground layers, they are aligned with red lines

--- core/src/com/paladinzzz/game/screens/GameScreen.java
+++ core/src/com/paladinzzz/game/screens/GameScreen.java
@@ -5,11 +5,19 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.Map;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -44,8 +52,24 @@ public class GameScreen implements Screen {
         this.worldMap = mapLoader.load("Worlds/TestWorld/TestWorld.tmx");
         this.mapRenderer = new OrthogonalTiledMapRenderer(worldMap);
         this.camera.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
-//        this.world = new World(new Vector2(0,0), true);
-//        this.debugRenderer = new Box2DDebugRenderer();
+        this.world = new World(new Vector2(0,0), true);
+        this.debugRenderer = new Box2DDebugRenderer();
+        debugRenderer.SHAPE_STATIC.set(1,0,0,1);
+
+        //Het maken van map objecten:
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+        for(MapObject object :worldMap.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            body = world.createBody(bdef);
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
 
     }
 
@@ -78,6 +102,9 @@ public class GameScreen implements Screen {
 
         //Render de map
         mapRenderer.render();
+
+        //Render de debug renderer lijntjes:
+        debugRenderer.render(world, camera.combined);
 
         //Zet de positie van de camera:
         game.batch.setProjectionMatrix(levelHUD.hudStage.getCamera().combined);
---
+++ core/src/com/paladinzzz/game/screens/worldobjects/ground.java
@@ -0,0 +1,13 @@
+package com.paladinzzz.game.screens.worldobjects;
+
+import com.badlogic.gdx.physics.box2d.BodyDef;
+
+/**
+ * Created by aaron on 21-Jun-17.
+ */
+
+public class ground {
+    public ground() {
+
+    }
+}
