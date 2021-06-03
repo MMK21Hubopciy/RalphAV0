067947df000b67faaa710278ae118fea35b2b840
MMK21Hubopciy <knoarb99@163.com>
2021-06-03 22:15:12 +0800

    Add files via upload

---
+++ worldobjects/groundObject.java
@@ -0,0 +1,51 @@
+package com.paladinzzz.game.screens.worldobjects;
+
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.paladinzzz.game.util.Constants;
+import com.paladinzzz.game.sprites.Mole;
+
+/**
+ * Created by aaron on 21-Jun-17.
+ *
+ * Editted by Je Broer Jasper de Lauwste XXX
+ */
+
+public class groundObject {
+
+    private Body body;
+    private Mole player;
+    private Rectangle rect;
+
+    public groundObject(World world, TiledMap map, Mole player) {
+        this.player = player;
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+
+        //De grond objecten zijn het 3e object in onze map editor, beginnend bij 0 is dat het 2e object
+        for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            this.rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+
+            //De positie delen we door 2, omdat libgdx begint in het midden van elke vorm.
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / Constants.PPM, (rect.getY() + rect.getHeight() / 2) / Constants.PPM);
+            this.body = world.createBody(bdef);
+            shape.setAsBox(rect.getWidth() / 2 / Constants.PPM, rect.getHeight() / 2 / Constants.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+    }
+
+    public boolean collides(){
+        return (this.body.getPosition().y / 2 >= player.body.getPosition().y);
+    }
+}
