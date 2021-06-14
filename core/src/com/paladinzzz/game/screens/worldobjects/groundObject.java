6609c08267666be51b8cbc09ae6b4e6f35738d7c
MMK21Hubopciy <knoarb99@163.com>
2021-06-15 06:27:06 +0800

    Added some comments and made the viewport FILL for now instead of FIT

--- core/src/com/paladinzzz/game/screens/worldobjects/groundObject.java
+++ core/src/com/paladinzzz/game/screens/worldobjects/groundObject.java
@@ -25,6 +25,8 @@ public class groundObject {
         for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
             bdef.type = BodyDef.BodyType.StaticBody;
+
+            //De positie delen we door 2, omdat libgdx begint in het midden van elke vorm.
             bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
             body = world.createBody(bdef);
             shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
