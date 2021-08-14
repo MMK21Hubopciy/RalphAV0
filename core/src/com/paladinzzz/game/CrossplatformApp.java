881dd869658a8be47fe5d9f0ecb5c5c768511c2e
MMK21Hubopciy <knoarb99@163.com>
2021-08-15 05:22:22 +0800

    added database

--- core/src/com/paladinzzz/game/CrossplatformApp.java
+++ core/src/com/paladinzzz/game/CrossplatformApp.java
@@ -2,6 +2,7 @@ package com.paladinzzz.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.paladinzzz.game.database.Database;
 import com.paladinzzz.game.screens.GameScreen;
 import com.paladinzzz.game.screens.MenuScreen;
 
@@ -12,6 +13,8 @@ public class CrossplatformApp extends Game {
 	public void create () {
 		batch = new SpriteBatch();
 		setScreen(new MenuScreen(this));
+		Database database = new Database();
+		database.getData(database.connect());
 
 	}
 
---
+++ core/src/com/paladinzzz/game/database/Database.java
@@ -0,0 +1,46 @@
+package com.paladinzzz.game.database;
+
+/**
+ * Created by Selim on 21/6/2017.
+ */
+
+
+import java.sql.*;
+
+public class Database{
+    private final String url = "jdbc:postgresql://localhost:5432/diggydb";
+    private final String user = "postgres";
+    private final String password = "kaas123";
+    private Connection conn;
+
+    public Connection connect(){
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        this.conn = conn;
+        return conn;
+    }
+
+    public void getData(Connection conn){
+        try {
+            Statement get = conn.createStatement();
+            String query = "Select * from player";
+            ResultSet rs = get.executeQuery(query);
+
+            while (rs.next()){
+                String firstName = rs.getString("name");
+                System.out.println(firstName);
+                String score = rs.getString("score");
+                System.out.println(score);
+            }
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+}
