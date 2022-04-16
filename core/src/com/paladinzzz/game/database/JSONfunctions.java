1bd456a8808dac5cd0de8d090af2b5018e3a3583
MMK21Hubopciy <knoarb99@163.com>
2022-04-16 23:42:58 +0800

    database working crossplatform

---
+++ core/src/com/paladinzzz/game/database/JSONfunctions.java
@@ -0,0 +1,47 @@
+package com.paladinzzz.game.database;
+
+
+import com.badlogic.gdx.utils.async.AsyncTask;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static com.badlogic.gdx.Input.Keys.R;
+
+/**
+ * Created by Selim on 27/6/2017.
+ */
+
+public class JSONfunctions{
+
+    public static final String MY_JSON ="MY_JSON";
+
+    private static final String api_url = "http://www.wemoney.nl/connect.php";
+
+    public String doInBackground() {
+
+        BufferedReader bufferedReader = null;
+        try {
+            URL url = new URL(api_url);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            StringBuilder sb = new StringBuilder();
+
+            bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String json;
+            while((json = bufferedReader.readLine())!= null){
+                sb.append(json+"\n");
+            }
+
+            return sb.toString().trim();
+
+        }catch(Exception e){
+            return null;
+        }
+
+    }
+}
