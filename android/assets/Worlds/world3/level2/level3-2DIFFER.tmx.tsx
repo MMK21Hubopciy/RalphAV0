fab8a5d9ccea1613b2e90b0b8fc02cbb58b26d91
MMK21Hubopciy <knoarb99@163.com>
2023-10-09 12:35:28 +0800

    World 3-2 Still in progress, Options fix

---
+++ android/assets/Worlds/world3/level2/level3-2DIFFER.tmx.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="level3-2Changed" tilewidth="16" tileheight="16" tilecount="11250" columns="250">
+ <image source="../../tileset/airplanebg.png" width="4000" height="720"/>
+</tileset>
